9ca4e9758000c773f32051088b98690b082e0c37
Jason Ghent <jasonghent1008@gmail.com>
2022-09-05 19:44:16 -0400

    bug in type in schema

--- tiktokclone/schemas/comment.js
+++ tiktokclone/schemas/comment.js
@@ -13,7 +13,7 @@ export default {
         {
             name: 'comment',
             title: 'Comment',
-            string: 'string'
+            type: 'string'
         }
     ]
 }
--- tiktokclone/schemas/schema.js
+++ tiktokclone/schemas/schema.js
@@ -4,6 +4,12 @@ import createSchema from 'part:@sanity/base/schema-creator'
 // Then import schema types from any plugins that might expose them
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 
+// import our schemas here
+import post from './post'
+import comment from './comment'
+import user from './user'
+import postedBy from './postedBy'
+
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
   // We name our schema
@@ -11,6 +17,7 @@ export default createSchema({
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
-    /* Your types here! */
+    // sanity will read these schemas and populate them inside sanity studio
+    post, comment, user, postedBy
   ]),
 })
