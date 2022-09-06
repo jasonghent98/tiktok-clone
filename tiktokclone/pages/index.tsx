c4bbf70b242d3cc360b94554ecf8519f23a077a1
Jason Ghent <jasonghent1008@gmail.com>
2022-09-05 20:32:31 -0400

    TS front-end project initialized + config settings

---
+++ tiktokclone/pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import React from 'react';
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+    return (
+        <div>
+            <Head>
+                <title>Create Next App</title>
+            </Head>
+            <div>test 123!</div>
+        </div>
+    )
+}
+
+export default Home
