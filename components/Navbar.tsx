443a1e61d8b0762c822c37f8bde555675ccd7059
Jason Ghent <jasonghent1008@gmail.com>
2022-09-07 09:26:52 -0400

    sidebar component icons placed

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,15 +5,18 @@ import {useRouter} from 'next/router'
 import {GoogleLogin, googleLogout} from '@react-oauth/google'
 import {AiOutlineLogout} from 'react-icons/ai'
 import {IoMdAdd} from 'react-icons/io'
+import Logo from '../utils/tiktok-logo.avif'
 
 export const Navbar = () => {
+  console.log(Logo)
   return (
-    <div>
-      <Link>
-        <div>
+    <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4'>
+      <Link href='/'>
+        <div className='w-[100px] md:w-[130px]>'>
           <Image 
           className='cursor-pointer'
-          src={}
+          src={Logo}
+          alt='tiktik'
           ></Image>
         </div>
       </Link>
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { NextPage } from 'next'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import GoogleLogin from 'react-google-login'
+import {AiFillHome, AiOutlineMenu} from 'react-icons/ai'
+import {ImCancelCircle} from 'react-icons/im'
 
 export const Sidebar = () => {
+  const [showSidebar, setShowSidebar] = useState(true)
+
+  const normalLink = 'flex items-center gap-3 hover:bg-primary p-3 justify-center xl:justify-start cursor-pointer font-semibold text-[#f51997] rounded'
   return (
-    <div>Sidebar</div>
+    <div>
+      <div className="block xl:hidden m-2 ml-4 mt-3 text-xl"
+            onClick={() => setShowSidebar((prevState) => !prevState)}>
+
+      </div>
+      {showSidebar ? <ImCancelCircle /> : <AiOutlineMenu />}
+      {showSidebar && (
+        <div className='xl:w-400 w-20 flex flex-col justify-start mb-10 border-4-2 border-gray-100 xl:border-0 p-3'>
+          <div className="xl:border-b-2 border-gray-200 xl:pb-4">
+            <Link href="/">
+              <div className={normalLink}>
+                <p className="text-2xl ">
+                  <AiFillHome />
+                </p>
+                <span className='text-xl hidden xl:block'>For You</span>
+              </div>
+              </Link>
+          </div>
+        </div>
+      )}
+
+        {/* check if user is logged in to render 'login' page or 'go to profile' */}
+
+        
+    </div>
   )
 }
 
