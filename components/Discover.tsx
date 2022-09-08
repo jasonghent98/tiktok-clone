c6739a2cf5f84526d37e604a7a92885b3b32d551
Jason Ghent <jasonghent1008@gmail.com>
2022-09-08 18:47:24 -0400

    created discover, footer, suggestedAccounts components for sidebar

---
+++ components/Discover.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { topics } from '../utils/constants'
+
+export const Discover = () => {
+  return (
+    <div className='xl-border-b-2 xl:border-gray-200 pb-6'>
+        <p className='text-gray-500 font-semibold m-3 mt-4 hidden xl:block'>
+            Popular Topics
+        </p>
+    </div>
+  )
+}
