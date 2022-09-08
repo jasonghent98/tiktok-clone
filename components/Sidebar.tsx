690995b2ee0410796fa5629944c61782127c779a
Jason Ghent <jasonghent1008@gmail.com>
2022-09-08 18:46:39 -0400

    further developing sidebar with discoever, footer, userProfile options etc...

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -5,11 +5,14 @@ import { useRouter } from 'next/router'
 import GoogleLogin from 'react-google-login'
 import {AiFillHome, AiOutlineMenu} from 'react-icons/ai'
 import {ImCancelCircle} from 'react-icons/im'
+import { Discover } from './Discover'
+import { Footer } from './Footer'
+import { SuggestedAccounts } from './SuggestedAccounts'
 
 export const Sidebar = () => {
   const [showSidebar, setShowSidebar] = useState(true)
 
-  const isLoggedIn = false
+  const userProfile = false
 
   useEffect(() => console.log(showSidebar), [showSidebar])
 
@@ -17,8 +20,11 @@ export const Sidebar = () => {
   return (
     <div>
       <div className="block xl:hidden m-2 ml-4 mt-3 text-xl"
-            onClick={() => setShowSidebar(prevState => !prevState)}>
+            onClick={() => setShowSidebar(prevState => !prevState)}
+      >
       {showSidebar ? <ImCancelCircle /> : <AiOutlineMenu />}
+      </div>
+
       {showSidebar && (
         <div className='xl:w-400 w-20 flex flex-col justify-start mb-10 border-4-2 border-gray-100 xl:border-0 p-3'>
           <div className="xl:border-b-2 border-gray-200 xl:pb-4">
@@ -32,16 +38,35 @@ export const Sidebar = () => {
               </Link>
           </div>
           {/* check if user is logged in to render 'login' page or 'go to profile' */}
-          {isLoggedIn && (
-            <div>
-              <p>Log in to like and comment on videos</p>
+          {!userProfile && (
+            <div className='px-2 py-4 hidden xl:block'>
+              <p className='text-gray-400 '>Log in to like and comment on videos</p>
+              <div className='pr-4'>
+                {/* requires a clientid, key to generate to login. also takes onSuccess handler, onFailure*/}
+                <GoogleLogin 
+                  clientId=''
+                  render={renderProps =>  
+                      <button 
+                          className='bg-white text-lg text-[#F51997] border-[1px] border-[#F51997] font-semibold px-6 py-3 rounded-md outline-none w-full mt-3 hover:text-white hover:bg-[#F51997]'
+                          onClick={renderProps.onClick} 
+                          disabled={renderProps.disabled}>
+                       Log in
+                      </button> 
+                  }
+                  onSuccess={() => {}}
+                  onFailure={() => {}}
+                />
+              </div>
             </div>
-          )}
+          )} 
+
+          <Discover />
+          <SuggestedAccounts />
+          <Footer />
         </div>   
       )}
     </div>
 
-    </div>
   )
 }
 
