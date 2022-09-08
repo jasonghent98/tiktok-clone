01f7106054948fb2eafa909c820844c130472d98
Jason Ghent <jasonghent1008@gmail.com>
2022-09-08 19:26:40 -0400

    conditional styling for sidebar discovery items

--- components/Discover.tsx
+++ components/Discover.tsx
@@ -7,6 +7,10 @@ export const Discover = () => {
 
     const activeTopicStyle = 'xl:border-2 hover:bg-primary xl:border-[#F51997] px-3 py-2 rounded xl:rounded-fill flex items-center gap-2 justify-center cursor-pointer text-[#F51997]'
     const topicStyle = 'xl:border-2 hover:bg-primary xl:border-gray-300 px-3 py-2 rounded xl:rounded-fill flex items-center gap-2 justify-center cursor-pointer text-black'
+
+    const router = useRouter()
+    const {topic} = router.query
+
   return (
     <div className='xl-border-b-2 xl:border-gray-200 pb-6'>
         <p className='text-gray-500 font-semibold m-3 mt-4 hidden xl:block'>
@@ -19,7 +23,7 @@ export const Discover = () => {
                 href={`/?topic=${item.name}`}
                 key={item.name}
             >
-                <div className={topicStyle}>
+                <div className={topic === item.name ? activeTopicStyle : topicStyle}>
                     <span className='font-bold text-2xl xl:text-md'>{item.icon}</span>
                     <span className='font-medium text-md hidden xl:block capitalize'>{item.name}</span>
                 </div>
