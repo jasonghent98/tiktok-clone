b8c8410971969e4a565f96831f6d76e7f6bd9b50
Jason Ghent <jasonghent1008@gmail.com>
2022-09-13 22:47:51 -0400

    styling for upload component

---
+++ pages/upload.tsx
@@ -0,0 +1,25 @@
+import React, {useState, useEffect} from 'react'
+import { useRouter } from 'next/router'
+import { FaCloudUploadAlt } from 'react-icons/fa'
+import {MdDelete} from 'react-icons/md'
+import axios from 'axios'
+import { client } from '../utils/client'
+import useAuthStore from '../store/authStore'
+
+const Upload = () => {
+  return (
+    <div className='flex w-full h-full'>
+        <div className='bg-white rounded-lg'>
+            <div>
+                <div>
+                    <p className='text-2xl font-bold'>Upload Video</p>
+                    <p className='text-md text-gray-400'>Post a video to your account</p>
+                </div>
+                <div className='rounded-xl border-4 border-gray-200 flex flex-col justify-center items-center outline-none mt-10 w-[260px] h-[460px] cursor-pointer hover:border-red-300 hover:bg-gray-100'></div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Upload
